15ac8ee6e53d64a5660adeac6f845456becf26b9
Keli Booster <kbooster17@gmail.com>
2022-04-19 06:36:13 +0000

    hash user password

--- src/db/models/userModel.js
+++ src/db/models/userModel.js
@@ -3,62 +3,61 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  user: {
-    username: {
-      type: String,
-      unique: true,
-      required: true,
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is not valid!");
+      }
     },
-    firstname: {
-      type: String,
-      required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error(
+          "Password should be 8 characters long. contain 1 lowercase and 1 uppercase character "
+        );
+      }
     },
-    lastname: {
-      type: String,
-      required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a postive number");
+      }
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error("Email is not valid!");
-        }
-      },
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 7,
-      trim: true,
-      validate(value) {
-        if (!validator.isStrongPassword(value)) {
-          throw new Error(
-            "Password should be 8 characters long. contain 1 lowercase and 1 uppercase character "
-          );
-        }
-      },
-    },
-    age: {
-      type: Number,
-      required: true,
-      validate(value) {
-        if (value < 0) {
-          throw new Error("Age must be a postive number");
-        }
-      },
-    },
-    createdAt: {
-      type: Date,
-      required: true,
-      default: Date.now(),
-    },
-    updatedAt: {
-      type: Date,
-    },
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
